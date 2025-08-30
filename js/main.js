818d83c9809a2752b1dd970b856e75f3cc6fb044
Lucas <alejandrolucascoloniodiaz@gmail.com>
2025-08-30 00:29:37 -0300

    Se agregan funciones al menu

--- js/main.js
+++ js/main.js
@@ -1,31 +1,53 @@
 
 let cancionesobjetos = [];
 
+function agregarproducto() { }
+function mostrarproductos() { }
+function buscarproducto() { }
+function eliminarproducto() { }
 
 
 
-function menuobjetos () {
+
+
+
+function menutiendaecommerce() {
     let opcion;
     do {
-        Option = prompt (
+        opcion = prompt(
+            "Eliga una opcion: \n" +
+            "1- Agregar producto\n" +
+            "2- Mostrar lista de productos \n" +
+            "3- Buscar producto \n" +
+            "4- Eliminar producto \n" +
+            "5- Salir"
+        );
+        switch (opcion) {
+            case '1':
+                agregarproducto()
+                break;
+            case '2':
+                mostrarproductos()
+                break;
+            case '3':
+                buscarproducto()
+                break;
+            case '4':
+                eliminarproducto()
+                break;
+            case '5': alert("Gracias por visitarnos")
+                break;
+
+            default: alert("Opcion incorrecta")
+        }
     } while (opcion != '5');
+}
 
 
 
 
-    menuobjetos ();
 
 
+menutiendaecommerce();
 
 
-//Ejemplo basico de arrays
-
-/* console.log("Ejemplo: array vacio");
-const canciones = ["Rayando el sol", "Persiana Americana", "En la ciudad de la furia"];
-console.log(canciones);
-console.table (canciones);
-
-console.log("-----------------------------------");
-for (let i=0 ; i< canciones.length; i= i+1){
-    console.log (canciones[i]);
-} */
