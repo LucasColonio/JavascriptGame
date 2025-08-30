572138ad2399221665069a8053cc875effc909b6
Lucas <alejandrolucascoloniodiaz@gmail.com>
2025-08-29 23:58:54 -0300

    Se agrega funciones

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+let cancionesobjetos = [];
+
+
+
+
+function menuobjetos () {
+    let opcion;
+    do {
+        Option = prompt (
+    } while (opcion != '5');
+
+
+
+
+    menuobjetos ();
+
+
+
+
+//Ejemplo basico de arrays
+
+/* console.log("Ejemplo: array vacio");
+const canciones = ["Rayando el sol", "Persiana Americana", "En la ciudad de la furia"];
+console.log(canciones);
+console.table (canciones);
+
+console.log("-----------------------------------");
+for (let i=0 ; i< canciones.length; i= i+1){
+    console.log (canciones[i]);
+} */
